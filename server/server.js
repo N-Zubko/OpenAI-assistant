79603cf5062c24f3efa8efdb63ffa6e9dc70bda3
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-02 22:42:43 -0600

    please work

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const responseRouter = require('./routes/responses');
 const path = require('path');
 const basicAuth = require('express-basic-auth');
