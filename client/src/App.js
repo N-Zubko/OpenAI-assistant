c5cdf08c46ef65a8d7b73068ea69b958d4efbafa
Nadia Zubko <ne.nadezda@gmail.com>
2023-01-04 01:14:57 -0700

    add Footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import AskQuestion from "./components/AskQuestion";
 import Results from "./components/Results";
+import Footer from "./components/Footer";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -17,6 +18,8 @@ function App() {
           <Route path="/question" element={<AskQuestion />}></Route>
           <Route path="/allResponses" element={<Results />}></Route>
         </Routes>
+        <Footer />
+
       </Router>
     </div>
   );
